731f4e45a4f899f51b17bfdf2738c184a7aeec7f
Ramin Anvar <ramina1964@gmail.com>
2024-10-25 02:16:49 +0200

    Moved event-related methods fields into a new partial class called MainViewModel.Events

---
+++ NQueen.GUI/ViewModels/MainViewModel.Events.cs
@@ -0,0 +1,44 @@
+﻿namespace NQueen.GUI.ViewModels;
+
+public sealed partial class MainViewModel
+{
+    private void OnProgressValueChanged(object sender, ProgressValueChangedEventArgs e) =>
+        ProgressValue = e.Value;
+
+    private void OnQueenPlaced(object sender, QueenPlacedEventArgs e)
+    {
+        var sol = new Solution([.. e.Solution], 1);
+        var positions = sol
+            .QueenList.Where(q => q > -1)
+            .Select((item, index) => new Position((sbyte)index, item)).ToList();
+
+        Chessboard.PlaceQueens(positions);
+    }
+
+    private void OnSolutionFound(object sender, SolutionFoundEventArgs e)
+    {
+        var id = ObservableSolutions.Count + 1;
+        var sol = new Solution([.. e.Solution], id);
+
+        _ = Application
+            .Current
+            .Dispatcher
+            .BeginInvoke(DispatcherPriority.Send, new Action(() => ObservableSolutions.Add(sol)));
+
+        SelectedSolution = sol;
+    }
+
+    private void SubscribeToSimulationEvents()
+    {
+        Solver.ProgressValueChanged += OnProgressValueChanged;
+        Solver.QueenPlaced += OnQueenPlaced;
+        Solver.SolutionFound += OnSolutionFound;
+    }
+
+    private void UnsubscribeFromSimulationEvents()
+    {
+        Solver.QueenPlaced -= OnQueenPlaced;
+        Solver.SolutionFound -= OnSolutionFound;
+        Solver.ProgressValueChanged -= OnProgressValueChanged;
+    }
+}
--- NQueen.GUI/ViewModels/MainViewModel.cs
+++ NQueen.GUI/ViewModels/MainViewModel.cs
@@ -380,51 +380,11 @@ public sealed partial class MainViewModel : ObservableObject, IDisposable
         }
     }
 
-    private void SubscribeToSimulationEvents()
-    {
-        Solver.ProgressValueChanged += OnProgressValueChanged;
-        Solver.QueenPlaced += OnQueenPlaced;
-        Solver.SolutionFound += OnSolutionFound;
-    }
-
-    private void UnsubscribeFromSimulationEvents()
-    {
-        Solver.QueenPlaced -= OnQueenPlaced;
-        Solver.SolutionFound -= OnSolutionFound;
-        Solver.ProgressValueChanged -= OnProgressValueChanged;
-    }
-
     private void UpdateButtonFunctionality()
     {
         SimulateCommand.NotifyCanExecuteChanged();
         CancelCommand.NotifyCanExecuteChanged();
         SaveCommand.NotifyCanExecuteChanged();
-    }
-
-    private void OnProgressValueChanged(object sender, ProgressValueChangedEventArgs e) =>
-        ProgressValue = e.Value;
-
-    private void OnQueenPlaced(object sender, QueenPlacedEventArgs e)
-    {
-        var sol = new Solution([.. e.Solution], 1);
-        var positions = sol
-            .QueenList.Where(q => q > -1)
-            .Select((item, index) => new Position((sbyte)index, item)).ToList();
-
-        Chessboard.PlaceQueens(positions);
-    }
-
-    private void OnSolutionFound(object sender, SolutionFoundEventArgs e)
-    {
-        var id = ObservableSolutions.Count + 1;
-        var sol = new Solution([.. e.Solution], id);
-
-        _ = Application
-            .Current
-            .Dispatcher
-            .BeginInvoke(DispatcherPriority.Send, new Action(() => ObservableSolutions.Add(sol)));
-
-        SelectedSolution = sol;
     }
 
     private void ExtractCorrectNoOfSols()
@@ -468,6 +428,8 @@ public sealed partial class MainViewModel : ObservableObject, IDisposable
 
     private bool CanCancel() => IsSimulating;
 
+    private bool CanSave() => IsIdle && IsOutputReady;
+
     private void Save()
     {
         var results = new ResultPresentation(SimulationResults);
@@ -476,8 +438,6 @@ public sealed partial class MainViewModel : ObservableObject, IDisposable
         MessageBox.Show(msg);
         IsIdle = true;
     }
-
-    private bool CanSave() => IsIdle && IsOutputReady;
     #endregion PrivateMethods
 
     #region PrivateFields
