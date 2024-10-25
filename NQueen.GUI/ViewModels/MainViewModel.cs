db5ebba7f48f486cec7ada7becc564dec0ae0f95
Ramin Anvar <ramina1964@gmail.com>
2024-10-25 03:19:41 +0200

    Added another partial MainViewModel called MainViewModel.Fields

--- NQueen.GUI/ViewModels/MainViewModel.cs
+++ NQueen.GUI/ViewModels/MainViewModel.cs
@@ -353,32 +353,4 @@ public sealed partial class MainViewModel : ObservableObject, IDisposable
         sols.ForEach(s => ObservableSolutions.Add(s));
     }
     #endregion PrivateMethods
-
-    #region PrivateFields
-    private double _progressValue;
-    private string _progressLabel;
-    private Visibility _progressLabelVisibility;
-    private Visibility _progressVisibility;
-    private IEnumerable<SolutionMode> _enumSolutionModes;
-    private IEnumerable<DisplayMode> _enumDisplayModes;
-    private static SimulationResults _simulationResults;
-    private int _delayInMilliseconds;
-    private string _noOfSoltions;
-    private string _elapsedTime;
-    private SolutionMode _solutionMode;
-    private DisplayMode _displayMode;
-    private string _boardSizeText;
-    private sbyte _boardSize;
-    private bool _isVisualized;
-    private bool _isValid;
-    private bool _isSingleRunning;
-    private bool _isIdle;
-    private bool _isSimulating;
-    private bool _isInInputMode;
-    private bool _isOutputReady;
-    private ISolver _solver;
-    private Solution _selectedSolution;
-    private string _solutionTitle;
-    private string _memoryUsage;
-    #endregion PrivateFields
 }
