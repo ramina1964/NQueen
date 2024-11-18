282533b8753131ef13e0d48653221f17eea0a54f
Ramin Anvar <ramina1964@gmail.com>
2024-11-18 13:33:23 +0100

    Removed comments and using statements from -xaml files

--- NQueen.GUI/Views/SimulationPanelUserControl.xaml.cs
+++ NQueen.GUI/Views/SimulationPanelUserControl.xaml.cs
@@ -1,28 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿namespace NQueen.GUI.Views;
 
-namespace NQueen.GUI.Views
+public partial class SimulationPanelUserControl : UserControl
 {
-    /// <summary>
-    /// Interaction logic for SimulationPanelUserControl.xaml
-    /// </summary>
-    public partial class SimulationPanelUserControl : UserControl
+    public SimulationPanelUserControl()
     {
-        public SimulationPanelUserControl()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
--- NQueen.GUI/Views/SolutionSummaryUserControl.xaml.cs
+++ NQueen.GUI/Views/SolutionSummaryUserControl.xaml.cs
@@ -1,28 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿namespace NQueen.GUI.Views;
 
-namespace NQueen.GUI.Views
+public partial class SolutionSummaryUserControl : UserControl
 {
-    /// <summary>
-    /// Interaction logic for SolutionSummaryUserControl.xaml
-    /// </summary>
-    public partial class SolutionSummaryUserControl : UserControl
+    public SolutionSummaryUserControl()
     {
-        public SolutionSummaryUserControl()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
