3c7428e78267ffa7d87764feb2679ee3becccb09
Ramin Anvar <ramina1964@gmail.com>
2024-11-22 19:51:46 +0100

    Formatted all views

--- NQueen.GUI/Infrastructure/ServiceCollectionExtensions.cs
+++ NQueen.GUI/Infrastructure/ServiceCollectionExtensions.cs
@@ -13,7 +13,7 @@ public static class ServiceCollectionExtensions
         services.AddTransient<Chessboard>();
         services.AddTransient<ChessboardUserControl>();
         services.AddSingleton<InputPanelUserControl>();
-        services.AddSingleton<StatusPanelUserControl>();
+        //services.AddSingleton<StatusPanelUserControl>();
 
         services.AddSingleton<MainViewModel>();
         services.AddSingleton<MainView>();
