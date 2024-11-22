917afe157cbecac96e586952e947b8c1ee1b873d
Ramin Anvar <ramina1964@gmail.com>
2024-11-22 01:20:53 +0100

    Small changesd

--- NQueen.GUI/ViewModels/MainViewModel.cs
+++ NQueen.GUI/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿namespace NQueen.GUI.ViewModels;
 
+// Todo: Fix a bug where Solution No. in the ListView of the GUI studders, Solution 1, Solution 1, ...
 public sealed partial class MainViewModel : ObservableObject, IDisposable
 {
     public MainViewModel() : this(new BackTrackingSolver(new SolutionManager()))
