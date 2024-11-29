71732902c8dec9c973fcc015e818d83a7f8d6127
Ramin Anvar <ramina1964@gmail.com>
2024-11-29 04:24:34 +0100

    Small changes

--- NQueen.Kernel/Utilities/Messages.cs
+++ NQueen.Kernel/Utilities/Messages.cs
@@ -4,7 +4,8 @@ public static class Messages
 {
     public const string InvalidSByteError = "Board size must be a valid integer.";
     public const string NoSolutionMessage = "No Solutions found. Try a larger board size!";
-    public const string ValueNullOrWhiteSpaceMsg = "Board size can not be null, empty or contain exclusively spaces.";
+    public const string ValueNullOrWhiteSpaceMsg =
+        "Board size can not be null, empty or contain exclusively spaces.";
 
     public static string SizeTooSmallMsg =>
         $"Board size must be greater than or equal to {BoardSettings.MinBoardSize}.";
