c9bf648f3e330aa56be69afbda168ee8dad957ed
Ramin Anvar <ramina1964@gmail.com>
2025-01-05 19:10:18 +0100

    Small changes

--- NQueen.GUI/Utils/Dimensions.cs
+++ NQueen.GUI/Utils/Dimensions.cs
@@ -4,7 +4,7 @@ public static class Dimensions
 {
     public static double ChessboardMinWidth => 140;
 
-    public static double ChessboardMaxHeight => 1050;
+    public static double SolutionsListMaxHeight => 1050;
 
     public static double ChessboardMaxWidth => 1050;
 
@@ -12,7 +12,7 @@ public static class Dimensions
 
     public static double SolutionTextBoxMinHeight => 40;
 
-    public static readonly GridLength ChessboardDefaultHeight = new(1000, GridUnitType.Pixel);
+    public static readonly GridLength SolutionsListDefaultHeight = new(1000, GridUnitType.Pixel);
 
     public static double GroupBoxMinHeight => 100;
 
@@ -28,5 +28,5 @@ public static class Dimensions
 
     public static double SliderHeight => 25;
 
-    public static readonly GridLength SolutionListTitleHeight = new(30, GridUnitType.Pixel);
+    public static readonly GridLength SolutionsListTitleHeight = new(30, GridUnitType.Pixel);
 }
