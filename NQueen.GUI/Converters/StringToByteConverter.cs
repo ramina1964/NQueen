eff2a1d9e8e597e76d5b90c650e645b95b532d2d
Ramin Anvar <ramina1964@gmail.com>
2025-01-18 21:31:38 +0100

    Added a new class called StringToByteConverter and used it to give an error message when board size is formatted correctly

---
+++ NQueen.GUI/Converters/StringToByteConverter.cs
@@ -0,0 +1,20 @@
+﻿namespace NQueen.GUI.Converters;
+
+public class StringToByteConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value?.ToString();
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string str && byte.TryParse(str, out byte result))
+        {
+            return result;
+        }
+
+        // Return a default value or handle the invalid input case
+        return DependencyProperty.UnsetValue;
+    }
+}
