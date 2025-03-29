645b0b81d3384bedabf65aed03ccd95fbcb39791
Ramin Anvar <ramin.anvar@cappelendamm.no>
2025-03-29 21:21:36 +0100

    Removed ByteToIntConverter and NullImageMultiConverter as it is not needed Also simplified BoardSizeConverter and NegativeConverter

--- NQueen.GUI/Converters/NegativeConverter.cs
+++ NQueen.GUI/Converters/NegativeConverter.cs
@@ -4,44 +4,28 @@ public class NegativeConverter : MarkupExtension, IValueConverter
 {
     public NegativeConverter() : base() { }
 
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-        return ReturnNegative(value);
-    }
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+        ReturnNegative(value);
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-        return ReturnNegative(value);
-    }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        ReturnNegative(value);
 
-    private static object ReturnNegative(object value)
-    {
-        object result = null;
-        Dictionary<Type, Action> @switch = new()
+    private static object ReturnNegative(object value) =>
+        value switch
         {
-            { typeof(bool), () => result = !(bool)value },
-            { typeof(byte), () => result = -1 * (byte)value },
-            { typeof(short), () => result = -1 * (short)value },
-            { typeof(int), () => result = -1 * (int)value },
-            { typeof(long), () => result = -1 * (long)value },
-            { typeof(float), () => result = -1f * (float)value },
-            { typeof(double), () => result = -1d * (double)value },
-            { typeof(decimal), () => result = -1m * (decimal)value }
+            bool b => !b,
+            byte b => -b,
+            short s => -s,
+            int i => -i,
+            long l => -l,
+            float f => -f,
+            double d => -d,
+            decimal m => -m,
+            _ => throw new NotImplementedException()
         };
 
-        @switch[value.GetType()]();
-
-        return
-            result
-            ?? throw new NotImplementedException();
-    }
-
-    public override object ProvideValue(IServiceProvider serviceProvider)
-    {
+    public override object ProvideValue(IServiceProvider serviceProvider) =>
         _converter ??= new NegativeConverter();
-
-        return _converter;
-    }
 
     private static NegativeConverter _converter = null;
 }
