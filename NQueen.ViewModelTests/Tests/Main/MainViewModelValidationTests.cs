9e1d3571c77ec4601239debec46bf13bb8afc9da
Ramin Anvar <ramina1964@gmail.com>
2025-05-20 13:04:37 +0200

    Improve board size validation tests for SolutionMode-dependent limits
    - Added BoardSizeText_Validation_ShouldReportLargeValues_BySolutionMode to test large board size inputs for each SolutionMode and verify correct error messages.
    - Removed SolutionMode-dependent large value cases from the generic validation test.
    - Ensured validation logic and error messages are accurately tested for all SolutionModes and edge cases.

--- NQueen.ViewModelTests/Tests/Main/MainViewModelValidationTests.cs
+++ NQueen.ViewModelTests/Tests/Main/MainViewModelValidationTests.cs
@@ -10,6 +10,11 @@ public class MainViewModelValidationTests
     [InlineData("-1", false, nameof(ErrorMessages.SizeTooSmallMsg))]
     [InlineData("abc", false, nameof(ErrorMessages.InvalidIntegerError))]
     [InlineData("", false, nameof(ErrorMessages.ValueNullOrWhiteSpaceMsg))]
+    [InlineData("1", true, null)]
+    [InlineData("17", true, null)]
+    [InlineData("18", true, null)]
+    [InlineData("8.0", false, nameof(ErrorMessages.InvalidIntegerError))]
+    [InlineData("4,5", false, nameof(ErrorMessages.InvalidIntegerError))]
     public void BoardSizeText_Validation_ShouldReportErrors(string? boardSizeText, bool isValid,
         string? expectedErrorKey)
     {
@@ -34,13 +39,68 @@ public class MainViewModelValidationTests
                 var expectedError = expectedErrorKey switch
                 {
                     nameof(ErrorMessages.SizeTooSmallMsg) => ErrorMessages.SizeTooSmallMsg,
-                    nameof(ErrorMessages.InvalidIntegerError) => ErrorMessages.InvalidIntegerError,
                     nameof(ErrorMessages.ValueNullOrWhiteSpaceMsg) => ErrorMessages.ValueNullOrWhiteSpaceMsg,
+                    nameof(ErrorMessages.InvalidIntegerError) => ErrorMessages.InvalidIntegerError,
                     _ => null
                 };
                 errors.Should().Contain(expectedError);
             }
         }
+    }
+
+    [Theory]
+    [InlineData("1000", SolutionMode.Single, false, nameof(ErrorMessages.SizeTooLargeForSingle))]
+    [InlineData("1000", SolutionMode.Unique, false, nameof(ErrorMessages.SizeTooLargeForUnique))]
+    [InlineData("1000", SolutionMode.All, false, nameof(ErrorMessages.SizeTooLargeForAll))]
+    [InlineData("18", SolutionMode.Unique, false, nameof(ErrorMessages.SizeTooLargeForUnique))]
+    [InlineData("18", SolutionMode.All, false, nameof(ErrorMessages.SizeTooLargeForAll))]
+    public void BoardSizeText_Validation_ShouldReportLargeValues_BySolutionMode(
+        string boardSizeText, SolutionMode solutionMode, bool isValid, string? expectedErrorKey)
+    {
+        var mainVm = TestHelpers.CreateMainViewModel();
+        mainVm.SolutionMode = solutionMode;
+        mainVm.BoardSizeText = boardSizeText;
+
+        var errors = mainVm.GetErrors(nameof(mainVm.BoardSizeText)).Cast<string>().ToList();
+
+        if (isValid)
+        {
+            errors.Should().BeEmpty();
+            mainVm.HasErrors.Should().BeFalse();
+        }
+        else
+        {
+            errors.Should().NotBeEmpty();
+            mainVm.HasErrors.Should().BeTrue();
+
+            if (expectedErrorKey != null)
+            {
+                var expectedError = expectedErrorKey switch
+                {
+                    nameof(ErrorMessages.SizeTooLargeForSingle) => ErrorMessages.SizeTooLargeForSingle,
+                    nameof(ErrorMessages.SizeTooLargeForUnique) => ErrorMessages.SizeTooLargeForUnique,
+                    nameof(ErrorMessages.SizeTooLargeForAll) => ErrorMessages.SizeTooLargeForAll,
+                    _ => null
+                };
+                errors.Should().Contain(expectedError);
+            }
+        }
+    }
+
+    [Fact]
+    public void BoardSizeText_Validation_ShouldRespectSolutionModeLimits()
+    {
+        var mainVm = TestHelpers.CreateMainViewModel();
+        mainVm.SolutionMode = SolutionMode.Unique;
+        mainVm.BoardSizeText = "21";
+
+        var errors = mainVm.GetErrors(nameof(mainVm.BoardSizeText)).Cast<string>().ToList();
+        errors.Should().Contain(ErrorMessages.SizeTooLargeForUnique);
+
+        mainVm.SolutionMode = SolutionMode.All;
+        mainVm.BoardSizeText = "18";
+        errors = [.. mainVm.GetErrors(nameof(mainVm.BoardSizeText)).Cast<string>()];
+        errors.Should().Contain(ErrorMessages.SizeTooLargeForAll);
     }
 
     [Fact]
