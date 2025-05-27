850ec971fbb7582c6164b931c024efda507ba1cc
Ramin Anvar <ramina1964@gmail.com>
2025-05-27 14:56:06 +0200

    Extracted a new method from Save_ShouldProcessSimulationResults() called AssertSavedContentProperties

--- NQueen.ViewModelTests/Helpers/AssertionHelpers.cs
+++ NQueen.ViewModelTests/Helpers/AssertionHelpers.cs
@@ -16,24 +16,39 @@ public static class AssertionHelpers
         mainVm.NoOfSolutions.Should().NotBe("0", TestConst.SolutionNumberZeroError);
     }
 
-    public static void AssertSavedContent(
-        string? savedContent, int boardSize, SimulationResults simulationResults)
+    public static void AssertSavedContentProperties(
+        string savedContent,
+        IDictionary<string, string> expectedProperties,
+        params string[] requiredKeysWithDynamicValues)
     {
-        savedContent.Should().NotBeNullOrEmpty(TestConst.ContentNotSavedError);
+        // Parse lines into a dictionary
+        var lines = savedContent.Split(['\r', '\n'],
+            StringSplitOptions.RemoveEmptyEntries);
 
-        // Validate the presence of key information
-        savedContent.Should().Contain(TestConst.BoardSizeLabel,
-            TestConst.BoardSizeLabelError);
+        var dict = new Dictionary<string, string>();
+        foreach (var line in lines)
+        {
+            var idx = line.IndexOf(':');
+            if (idx > 0)
+            {
+                var key = line.Substring(0, idx).Trim();
+                var value = line.Substring(idx + 1).Trim();
+                dict[key] = value;
+            }
+        }
 
-        savedContent.Should().Contain(TestConst.NoOfSolutionsLabel,
-            TestConst.NoOfSolsLabelError);
+        // Assert all expected properties
+        foreach (var kvp in expectedProperties)
+        {
+            dict.Should().ContainKey(kvp.Key, $"Missing property: {kvp.Key}");
+            dict[kvp.Key].Should().Be(kvp.Value, $"Property '{kvp.Key}' should have value '{kvp.Value}'");
+        }
 
-        savedContent.Should().Contain(TestConst.ElapsedTimeLabel,
-            TestConst.ElapsedTimeLabelError);
-
-        // Validate the values
-        savedContent.Should().Contain(boardSize.ToString(), TestConst.BoardSizeValueError);
-        savedContent.Should().Contain(simulationResults.Solutions.Count().ToString(),
-            TestConst.BoardSizeValueError);
+        // Assert required keys with dynamic values exist
+        foreach (var key in requiredKeysWithDynamicValues)
+        {
+            dict.Should().ContainKey(key, $"Missing property: {key}");
+            dict[key].Should().NotBeNullOrWhiteSpace($"Property '{key}' should have a value");
+        }
     }
 }
--- NQueen.ViewModelTests/Tests/Main/MainViewModelPositiveTests.cs
+++ NQueen.ViewModelTests/Tests/Main/MainViewModelPositiveTests.cs
@@ -90,8 +90,11 @@ public class MainViewModelPositiveTests : IDisposable
         // Assert
         mockSaveFileDialogService.WasCalled.Should().BeTrue(TestConst.SaveDialogNotShownError);
 
-        AssertionHelpers.AssertSavedContent(
-            mockSaveFileDialogService.SavedContent, boardSize, mainVm.SimulationResults);
+        var savedContent = mockSaveFileDialogService.SavedContent;
+        savedContent.Should().NotBeNullOrEmpty(TestConst.ContentNotSavedError);
+
+        AssertSavedContentProperties(savedContent!, boardSize,
+            solutionMode, mainVm.SimulationResults);
     }
 
     [Fact]
@@ -132,6 +135,51 @@ public class MainViewModelPositiveTests : IDisposable
     //    // After completion, IsSingleRunning should be false
     //    mainVm.IsSingleRunning.Should().BeFalse();
     //}
+
+    private void AssertSavedContentProperties(
+        string savedContent,
+        int boardSize,
+        SolutionMode solutionMode,
+        SimulationResults simulationResults)
+    {
+        var lines = savedContent.Split(['\r', '\n'],
+            StringSplitOptions.RemoveEmptyEntries);
+
+        var dict = new Dictionary<string, string>();
+        foreach (var line in lines)
+        {
+            var idx = line.IndexOf(':');
+            if (idx > 0)
+            {
+                var key = line.Substring(0, idx).Trim();
+                var value = line.Substring(idx + 1).Trim();
+                dict[key] = value;
+            }
+        }
+
+        dict.Should().ContainKey("Date && Time");
+        dict["Date && Time"].Should().NotBeNullOrWhiteSpace();
+
+        dict.Should().ContainKey("Board Size");
+        dict["Board Size"].Should().Be(boardSize.ToString());
+
+        dict.Should().ContainKey("SolutionMode");
+        dict["SolutionMode"].Should().Be(solutionMode.ToString());
+
+        dict.Should().ContainKey("Number of Solutions");
+        dict["Number of Solutions"].Should()
+            .Be(simulationResults.NoOfSolutions.ToString());
+
+        dict.Should().ContainKey("Max Number of Solutions Included");
+        dict["Max Number of Solutions Included"].Should()
+            .Be(SimulationSettings.MaxNoOfSolutionsInOutput.ToString());
+
+        dict.Should().ContainKey("Elapsed Time");
+        dict["Elapsed Time"].Should().Contain("seconds");
+
+        dict.Should().ContainKey("Memory Usage");
+        dict["Memory Usage"].Should().Contain("MB");
+    }
 
     public void Dispose() => _serviceProvider.Dispose();
 
