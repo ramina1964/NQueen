7d70da7f8a5af4786650b9812cdcfd1a6b9c2379
Ramin Anvar <ramina1964@gmail.com>
2025-06-07 19:42:08 +0200

    Copy BackTrackingSolver and dependencies to NQueen.NextGenKernel; update references and usings

---
+++ NQueen.NextGenKernel/Usings.cs
@@ -0,0 +1,7 @@
+﻿global using NQueen.Domain.Enums;
+global using NQueen.Domain.EventArgs;
+global using NQueen.Domain.Interfaces;
+global using NQueen.Domain.Models;
+global using NQueen.Domain.Settings;
+global using NQueen.Domain.Utils;
+global using System.Diagnostics;
