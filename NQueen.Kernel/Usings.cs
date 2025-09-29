b6cbd49440a5a7c2e411d82e79620170d97182a7
Ramin Anvar <ramina1964@gmail.com>
2025-09-30 00:13:09 +0200

    Removed obsolete projects in Windows Explorer.

--- NQueen.Kernel/Usings.cs
+++
@@ -1,7 +0,0 @@
-﻿global using Microsoft.Extensions.DependencyInjection;
-global using NQueen.Domain.Enums;
-global using NQueen.Domain.Interfaces;
-global using NQueen.Domain.Models;
-global using NQueen.Domain.Utils;
-global using NQueen.Kernel.Solvers;
-global using System.Diagnostics;
--- NQueen.NextGenKernel/Usings.cs
+++
@@ -1,9 +0,0 @@
-﻿global using Microsoft.Extensions.DependencyInjection;
-global using NQueen.Domain.Enums;
-global using NQueen.Domain.EventArgs;
-global using NQueen.Domain.Interfaces;
-global using NQueen.Domain.Models;
-global using NQueen.Domain.Settings;
-global using NQueen.Domain.Utils;
-global using NQueen.NextGenKernel.Solvers;
-global using System.Diagnostics;
