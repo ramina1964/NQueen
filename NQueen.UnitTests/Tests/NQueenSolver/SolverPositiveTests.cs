c4cbefa86b394a9dc248442897810a542480dc63
Ramin Anvar <ramina1964@gmail.com>
2025-10-14 20:19:21 +0200

    Implemented selective verification: full solution set comparison only for N <= 6; larger boards now assert counts only.

--- NQueen.UnitTests/Tests/NQueenSolver/SolverPositiveTests.cs
+++ NQueen.UnitTests/Tests/NQueenSolver/SolverPositiveTests.cs
@@ -7,68 +7,67 @@ public class SolverPositiveTests
 {
     private readonly ISolverBackEnd _solver;
 
+    private const int ExhaustiveSetVerificationMaxSize = 6; // full solution set equivalence only up to this size
+
     public SolverPositiveTests(SolverBackEndFixture fixture)
     {
         _solver = fixture.Sut;
     }
 
     [Theory]
-    [MemberData(nameof(NQueenTestSets.SolverShouldGenerateOneSingleSolutionData),
-        MemberType = typeof(NQueenTestSets))]
-    public async Task SolverShouldGenerateOneSingleSolution(
-        int boardSize, SolutionMode solutionMode)
+    [MemberData(nameof(NQueenTestSets.SolverShouldGenerateOneSingleSolutionData), MemberType = typeof(NQueenTestSets))]
+    public async Task SolverShouldGenerateOneSingleSolution(int boardSize, SolutionMode solutionMode)
     {
         var simContext = new SimulationContext(boardSize, solutionMode, DisplayMode.Hide);
         var expectedSolutions = TestBase.FetchExpectedSols(simContext);
         Assert.Single(expectedSolutions);
 
         var actualResults = await _solver.GetSimResultsAsync(simContext);
-        var actualSolutionsList = actualResults.Solutions
-            .Select(solution => solution.QueenPositions.ToArray())
-            .ToList();
-
-        actualSolutionsList.Should().ContainSingle()
-            .Which.Should().BeEquivalentTo(expectedSolutions.First());
+        actualResults.Solutions.Should().ContainSingle();
+        var actual = actualResults.Solutions[0].QueenPositions.ToArray();
+        actual.Should().BeEquivalentTo(expectedSolutions[0]);
     }
 
     [Theory]
-    [MemberData(nameof(NQueenTestSets.SolverShouldGenerateCorrectListOfUniqueSolutions),
-        MemberType = typeof(NQueenTestSets))]
-    public async Task SolverShouldGenerateCorrectListOfUniqueSolutions(
-        int boardSize, SolutionMode solutionMode)
+    [MemberData(nameof(NQueenTestSets.SolverShouldGenerateCorrectListOfUniqueSolutions), MemberType = typeof(NQueenTestSets))]
+    public async Task SolverShouldGenerateCorrectListOfUniqueSolutions(int boardSize, SolutionMode solutionMode)
     {
         var simContext = new SimulationContext(boardSize, solutionMode, DisplayMode.Hide);
         var expectedSolutions = TestBase.FetchExpectedSols(simContext);
-
         var actualResults = await _solver.GetSimResultsAsync(simContext);
-        var actualSolutionsList = actualResults.Solutions
-            .Select(solution => solution.QueenPositions.ToArray())
-            .ToList();
 
-        SolutionAssertions.AssertSolutionsSetEquivalent(
-            actualSolutionsList,
-            expectedSolutions,
-            $"N={boardSize} {solutionMode}");
+        if (boardSize <= ExhaustiveSetVerificationMaxSize)
+        {
+            var actualSolutionsList = actualResults.Solutions
+                .Select(s => s.QueenPositions.ToArray())
+                .ToList();
+            SolutionAssertions.AssertSolutionsSetEquivalent(actualSolutionsList, expectedSolutions, $"N={boardSize} {solutionMode}");
+        }
+        else
+        {
+            actualResults.SolutionsCount.Should().Be((ulong)expectedSolutions.Count, $"Unique count should match expected for N={boardSize}");
+        }
     }
 
     [Theory]
-    [MemberData(nameof(NQueenTestSets.SolverShouldGenerateCorrectListOfAllSolutionsData),
-        MemberType = typeof(NQueenTestSets))]
-    public async Task SolverShouldGenerateCorrectListOfAllSolutions(
-        int boardSize, SolutionMode solutionMode)
+    [MemberData(nameof(NQueenTestSets.SolverShouldGenerateCorrectListOfAllSolutionsData), MemberType = typeof(NQueenTestSets))]
+    public async Task SolverShouldGenerateCorrectListOfAllSolutions(int boardSize, SolutionMode solutionMode)
     {
         var simContext = new SimulationContext(boardSize, solutionMode, DisplayMode.Hide);
         var expectedSolutions = TestBase.FetchExpectedSols(simContext);
-
         var actualResults = await _solver.GetSimResultsAsync(simContext);
-        var actualSolutionsList = actualResults.Solutions
-            .Select(solution => solution.QueenPositions.ToArray())
-            .ToList();
 
-        SolutionAssertions.AssertSolutionsSetEquivalent(
-            actualSolutionsList,
-            expectedSolutions,
-            $"N={boardSize} {solutionMode}");
+        if (boardSize <= ExhaustiveSetVerificationMaxSize)
+        {
+            var actualSolutionsList = actualResults.Solutions
+                .Select(s => s.QueenPositions.ToArray())
+                .ToList();
+            SolutionAssertions.AssertSolutionsSetEquivalent(actualSolutionsList, expectedSolutions, $"N={boardSize} {solutionMode}");
+        }
+        else
+        {
+            actualResults.SolutionsCount.Should().Be((ulong)expectedSolutions.Count, $"All-solutions count should match expected for N={boardSize}");
+        }
     }
 }
 
